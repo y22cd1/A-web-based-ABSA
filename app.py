849e1bdd134de12e9a304c5ee070d22014f90320
y22cd1 <manikantaakasapu@gmail.com>
2026-04-23 18:50:57 +0530

    ready for render deployment

--- app.py
+++ app.py
@@ -1391,4 +1391,4 @@ def logout():
     return redirect(url_for("login"))
 # -------- Main -------- #
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True, port=int(os.environ.get("PORT", 5000)))
